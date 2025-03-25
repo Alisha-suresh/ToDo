3ade5d8b8d5be437a7bf8adc3a6b03e42000474d
Alisha-suresh <alishasuresh142@gmail.com>
2025-03-25 02:41:13 +0000

    removing files

--- obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b27997a2f28b0ca6e79e50e9b5091bff53f453")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
